7eb54ecf08dea1962e3ae4bc8092ae2a1d505bd2
Léo Willian Kölln <leokolln@gmail.com>
2018-03-25 23:21:42 -0300

    Documented class and it's methods.

--- include/cppaikit/fsm/State.hpp
+++ include/cppaikit/fsm/State.hpp
@@ -2,6 +2,13 @@
 
 namespace aikit::fsm {
 
+/**
+ * The base class for the state of a Finite State Machine.
+ * All state to be used on a FSM should inherit from this class.
+ * @tparam TUpdateData Type for the data being passed on update() and fsm::State::update(). Defaults to int.
+ * @note The template parameter \a TUpdateData must be the same as the one declared for the FSM.
+ * @sa fsm::FSM
+ */
 template<typename TUpdateData = int>
 class State {
  public:
@@ -9,8 +16,21 @@ class State {
 
   virtual ~State() = default;
 
+  /**
+   * Method called when a FSM transition to this state.
+   * @sa fsm::FSM::transitionTo()
+   */
   virtual void onEnter() {};
+  /**
+   * Method called when a FSM transition from this state.
+   * @sa fsm::FSM::transitionTo()
+   */
   virtual void onExit() {};
+  /**
+   * Update the state. This method is called by the FSM when it is updated.
+   * @param updateData The data to be used during the update.
+   * @sa fsm::FSM::update()
+   */
   virtual void update(TUpdateData updateData) = 0;
 };
 
