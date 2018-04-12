c61115956316d16294ae59eb01557103986bfb6c
leokolln <leokolln@gmail.com>
2018-04-12 03:54:28 -0300

    Travis CI streamlined, now control Coveralls variable by .travis.yml and compiler bin names are now closer to the observed on logs during Travis build.

--- ci/travis-environment.sh
+++ ci/travis-environment.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$COMPILER" == "g++-7" ]; then
-  IS_COVERAGE_BUILD=1
-else
-  IS_COVERAGE_BUILD=0
-fi
-
-export IS_COVERAGE_BUILD
+env
