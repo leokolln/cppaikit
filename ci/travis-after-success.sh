1e9cc0e2a7dcc38e3a3cb5b794c3dc7dde177fe9
leokolln <leokolln@gmail.com>
2018-04-12 04:18:13 -0300

    Fixed missing option to exclude folder during Coveralls call.

--- ci/travis-after-success.sh
+++ ci/travis-after-success.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [ "$IS_COVERAGE_BUILD" == 1 ]; then
-  coveralls --exclude deps examples --gcov-options '\-lp'
+  coveralls --exclude deps --exclude examples --gcov-options '\-lp'
 fi
