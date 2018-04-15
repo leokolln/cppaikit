7f451d9ebca3f2aa59bae76df9bc1706dc93a4d6
Léo Willian Kölln <leokolln@gmail.com>
2018-04-15 02:44:23 -0300

    Simplified Coveralls report submit call.

--- ci/travis-after-success.sh
+++ ci/travis-after-success.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [ "$IS_COVERAGE_BUILD" == 1 ]; then
-  coveralls --exclude deps --exclude examples --gcov-options '\-lp'
+  coveralls -E deps -E examples --gcov-options '\-lp'
 fi
